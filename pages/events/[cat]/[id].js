4a293bce7d1b74dde370fef8251e5c9f235a6683
li,xiaokun <leeli@iu.edu>
2022-12-22 21:56:15 -0500

    done with all routes

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,13 +1,49 @@
-const EventPage = () => 
+import Link from 'next/link'
+import Image from 'next/image'
+const EventPage = ({data}) => 
 {
     return (
         <div>
-            <h1>Our Single Event</h1>
-
+            <Image width={1000} height={500} alt={data.title} src={data.image}/>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
         </div>
     )
 }
 
 export default EventPage;
+
+
+export async function getStaticPaths()
+{
+    const {allEvents} = await import ('../../../data/data.json');
+    console.log (allEvents);
+    const allPaths = allEvents.map((path)=> {
+        return {
+            params: {
+            cat: path.city,
+            id: path.id,
+        },
+    };
+    });
+
+    return {
+        paths: allPaths,
+        fallback: false,
+    } 
+}
+
+
+export async function getStaticProps (context)
+{
+    console.log(context);
+    const id = context.params.id;
+    const {allEvents} = await import ('../../../data/data.json');
+    const eventData = allEvents.find(ev=> id ===ev.id);
+
+    return {
+        props: {data: eventData},
+    }
+}
 
 
